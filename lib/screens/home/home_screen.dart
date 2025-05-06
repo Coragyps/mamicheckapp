1d1cdefe2423d7609db4b495afb4ef5a22b97f24
Arturoqq <joserojasr23@gmail.com>
2025-05-05 23:54:57 -0500

    Navegacion Paginas

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,45 +1,64 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:mamicheckapp/services/auth_service.dart';
+import 'package:mamicheckapp/screens/screens.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<List<dynamic>> _pages = [
+    [FeedScreen(), 'Inicio', Icon(Icons.home)],
+    [Placeholder(), 'Historial', Icon(Icons.person)],
+    [Placeholder(), 'Notificaciones', Icon(Icons.settings)],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _titlebar(context),
+      drawer: _menu(context),
       body: _body(context),
+      bottomNavigationBar: _navbar(context),
     );
   }
 
   PreferredSizeWidget _titlebar(BuildContext context) {
     return AppBar(
       centerTitle: true,
-      title: Text(FirebaseAuth.instance.currentUser!.email!.toString(),),
+      title: Text(_pages[_selectedIndex][1] as String)
+    );
+  }
+
+  Widget _menu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: const [
+          DrawerHeader(child: Text('data')),
+          ListTile(title: Text('data1'),)
+        ],
+      ),
     );
   }
 
   Widget _body(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Hello👋'),
-              const SizedBox(height: 16,),
-              ElevatedButton(
-                onPressed: () async {
-                  await AuthService().signout();
-                  Navigator.pushReplacementNamed(context, "LoginScreen");
-                },
-                child: const Text('Sign Out'),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return IndexedStack(
+      index: _selectedIndex,
+      children: _pages.map((item) => item[0] as Widget).toList(),
+    );
+  } 
+
+  Widget _navbar(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: _selectedIndex,
+      onDestinationSelected: (index) {
+        if (_selectedIndex != index) {setState(() => _selectedIndex = index);}
+      },
+      destinations: _pages.map((item) => NavigationDestination(icon: item[2], label: item[1])).toList(),
     );
   }
 }
