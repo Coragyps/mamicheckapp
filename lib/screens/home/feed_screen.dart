93e562c3f611d5196a3a65cfb3a6636a8d084ff8
Arturoqq <joserojasr23@gmail.com>
2025-05-07 14:21:17 -0500

    mas paneles

--- lib/screens/home/feed_screen.dart
+++ lib/screens/home/feed_screen.dart
@@ -1,44 +1,69 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:mamicheckapp/services/auth_service.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _titlebar(context),
-      body: _body(context),
+    return Center(
+      child: Text('Hello👋'),
     );
   }
+}
 
-  PreferredSizeWidget _titlebar(BuildContext context) {
-    return AppBar(
-      centerTitle: true,
-      title: Text(FirebaseAuth.instance.currentUser!.email!.toString(),),
-    );
-  }
+class TrackingScreen extends StatelessWidget {
+  const TrackingScreen({super.key});
 
-  Widget _body(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Hello👋'),
-              const SizedBox(height: 16,),
-              ElevatedButton(
-                onPressed: () async {
-                  await AuthService().signout();
-                  Navigator.pushReplacementNamed(context, "LoginScreen");
-                },
-                child: const Text('Sign Out'),
-              ),
+  @override
+  Widget build(BuildContext context) {
+    return const DefaultTabController(
+      length: 3,
+      child: Column(
+        children: [
+          const TabBar(
+            tabs: [
+              Tab(text: 'Alertas'),
+              Tab(text: 'Invitaciones'),
+              Tab(text: 'Opiniones'),
             ],
           ),
-        ),
+          Expanded(
+            child: TabBarView(
+              children: [
+                Placeholder(),
+                Placeholder(),
+                Placeholder(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NotificacionesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          const TabBar(
+            tabs: [
+              Tab(text: 'Personal'),
+              Tab(text: 'Seguidos'),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              children: [
+                Placeholder(),
+                ListView.builder(itemCount: 30,itemBuilder: (_, i) => ListTile(title: Text('Item $i')),),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mamicheckapp/navigation/arguments.dart';
 import 'package:mamicheckapp/screens/screens.dart';
+import 'package:mamicheckapp/services/auth_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,36 +13,91 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  final firebaseuser = FirebaseAuth.instance.currentUser;
 
   final List<List<dynamic>> _pages = [
-    [FeedScreen(), 'Inicio', Icon(Icons.home)],
-    [Placeholder(), 'Historial', Icon(Icons.person)],
-    [Placeholder(), 'Notificaciones', Icon(Icons.settings)],
+    [NotificacionesPage(), 'Inicio', Icon(Icons.home_outlined), Icon(Icons.home)],
+    [ListView.builder(itemCount: 30,itemBuilder: (_, i) => ListTile(title: Text('Item $i')),), 'Historial', Icon(Icons.analytics_outlined), Icon(Icons.analytics)],
+    [FeedScreen(), 'Contacto', Icon(Icons.groups_outlined), Icon(Icons.groups)],
+    //[Placeholder(), 'Notificaciones', Icon(Icons.settings)],
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _titlebar(context),
-      drawer: _menu(context),
+      appBar: _titlebar(context, firebaseuser),
+      drawer: _menu(context, firebaseuser),
       body: _body(context),
       bottomNavigationBar: _navbar(context),
+      floatingActionButton: _add(context),
     );
   }
 
-  PreferredSizeWidget _titlebar(BuildContext context) {
+  PreferredSizeWidget _titlebar(BuildContext context, firebaseuser) {
     return AppBar(
-      centerTitle: true,
-      title: Text(_pages[_selectedIndex][1] as String)
+      //title: Text(_pages[_selectedIndex][1] as String),
+      title: Text('MamiCheck'),
+      actions: [
+        IconButton(
+          onPressed: () {}, 
+          icon: const Icon(Icons.notifications)
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 12),
+          child: GestureDetector(
+            onTap: () {},
+            child: CircleAvatar(
+              child: Text(firebaseuser.email.toString().isNotEmpty ? firebaseuser.email.toString()[0].toUpperCase() : '??'),
+            )
+          ),
+        )
+      ],
     );
   }
 
-  Widget _menu(BuildContext context) {
+  Widget _menu(BuildContext context, firebaseuser) {
     return Drawer(
       child: ListView(
-        children: const [
-          DrawerHeader(child: Text('data')),
-          ListTile(title: Text('data1'),)
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(firebaseuser.email.toString()),
+            accountEmail: Text(firebaseuser.email.toString()),
+            currentAccountPicture: CircleAvatar(
+              //backgroundImage: AssetImage('assets/profile.jpg'), // Usa NetworkImage si es online
+              child: Text(firebaseuser.email.toString().isNotEmpty ? firebaseuser.email.toString()[0].toUpperCase() : '??'),
+            ),
+          ),
+          const ListTile(
+            leading: Icon(Icons.medical_services),
+            title: Text('Mis Mediciones'),
+          ),
+          const ListTile(
+            leading: Icon(Icons.medical_services),
+            title: Text('Mis Mediciones'),
+          ),
+          const ListTile(
+            leading: Icon(Icons.help),
+            title: Text('Ayuda'),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(Icons.local_fire_department),
+            title: Text('Probar API'),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Configuración'),
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Cerrar Sesión'),
+            onTap: () async {
+              await AuthService().signout();
+              Navigator.pushNamedAndRemoveUntil(context, 'LoginScreen', (Route<dynamic> route) => false);
+            } 
+          ),
         ],
       ),
     );
@@ -58,7 +116,16 @@ class _HomeScreenState extends State<HomeScreen> {
       onDestinationSelected: (index) {
         if (_selectedIndex != index) {setState(() => _selectedIndex = index);}
       },
-      destinations: _pages.map((item) => NavigationDestination(icon: item[2], label: item[1])).toList(),
+      destinations: _pages.map((item) => NavigationDestination(icon: item[2], selectedIcon: item[3], label: item[1])).toList(),
+    );
+  }
+
+  Widget _add(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.pushNamed(context, 'MyHomePage', arguments: MyHomePageArguments(title: 'fab button'));
+      },
+      child: const Icon(Icons.add),
     );
   }
 }
